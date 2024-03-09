2dae246953e81c1f9401bcbd0a534bedde857505
Colin Huber <cbhuber@gmail.com>
2024-03-08 21:14:31 -0700

    fix: Spelling and black formatting

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,22 +6,25 @@ by 5, "FizzBuzz" if divisible by both, and the integer otherwise.
 
 # ----------------------------------------------------------------------------------------------
 
+
 def get_number() -> str:
     """
     Asks the user to input an integer number and returns it as a string.
 
-    This function prompts the user to enter an integer number through the console, 
-    and then returns the user input as a string. If the input is not a valid integer, 
+    This function prompts the user to enter an integer number through the console,
+    and then returns the user input as a string. If the input is not a valid integer,
     it will be returned as a string regardless.
 
     Returns:
     - A string that represents the integer number entered by the user.
     """
 
-    n = input('Enter a postive integer number: ')
+    n = input("Enter a positive integer number: ")
     return n
 
+
 # ----------------------------------------------------------------------------------------------
+
 
 def check_number(n) -> int:
     """Converts the input parameter to an integer and returns it if it is a valid integer.
@@ -41,21 +44,23 @@ def check_number(n) -> int:
         # Check for float
         if isinstance(n, float):
             raise ValueError
-        
+
         # Cast string to int
         n = int(n)
 
         # Check positive
         if n <= 0:
             raise ValueError
-        
+
     except ValueError:
         print(f"ERROR: {n} is not an integer or positive.")
         raise
 
     return n
 
+
 # ----------------------------------------------------------------------------------------------
+
 
 def do_single_fb(n) -> str:
     """Returns a string containing 'Fizz' if n is divisible by 3, 'Buzz' if n is
@@ -70,24 +75,25 @@ def do_single_fb(n) -> str:
         representation of n.
     """
 
-    output = ''
+    output = ""
 
     if n % 3 == 0:
-        output += 'Fizz'
+        output += "Fizz"
     if n % 5 == 0:
-        output += 'Buzz'
+        output += "Buzz"
     if not output:
         output = str(n)
 
     return output
 
+
 # ----------------------------------------------------------------------------------------------
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     n = get_number()
 
     n = check_number(n)
 
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         print(do_single_fb(i))
